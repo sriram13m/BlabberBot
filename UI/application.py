8f9a647a4864419015dd6f10ad9e260fa60dee9b
Sriram <sriram13k@gmail.com>
2018-10-28 15:43:37 +0530

    Added UI features

--- UI/application.py
+++ UI/application.py
@@ -1,19 +1,20 @@
 from flask import Flask, session , render_template ,request
+#import blabberbot
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", celebrity = celebrity)
 
 @app.route("/tweet" , methods=["POST"])
 def tweet():
     try:
-        celebrity = request.form.get("celebrity")
-        print(celebrity)
+        celebrity_name = request.form.get("celebrity")
+        print(celebrity_name)
     except ValueError:
         return render_template("error.html",error="Please select a celebrity")
 
     #TODO import Markov library enter the celeb and get the tweet
 
-    return render_template("tweet.html" , tweet = tweet) 
+    return render_template("tweet.html" , tweet = tweet)
