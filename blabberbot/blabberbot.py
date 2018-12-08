697ac74b85ddd88ce15da9346e5defe081e7aaa1
Sriram <sriram13k@gmail.com>
2018-12-08 15:07:43 +0530

    Pathfile fix

--- blabberbot/blabberbot.py
+++ blabberbot/blabberbot.py
@@ -1,5 +1,5 @@
 import markovify
-
+from pathlib import Path
 class Blabberbot():
     def __init__(self,filepath):
         with open(filepath) as file:
@@ -10,6 +10,8 @@ class Blabberbot():
     def generate_tweet(self,n):
         return self.text_model.make_short_sentence(n)
 
+folder = Path.cwd().parent / "Tweets"
+
 celebs = {}
-celebs["ElonMusk"] = "../Tweets/ElonMusk.txt"
-celebs["Tesla"] = "../Tweets/Tesla.txt"
+celebs["ElonMusk"] = folder / "ElonMusk.txt"
+celebs["Tesla"] = folder / "Tesla.txt"
