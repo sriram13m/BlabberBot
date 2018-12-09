cc8ea893806584c8d2ca9535f59d2d0578c2fa85
Sriram <sriram13k@gmail.com>
2018-12-09 22:53:56 +0530

    Added a better model

--- Tweets/tweet.py
+++ Tweets/tweet.py
@@ -5,7 +5,7 @@ import sys,os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'blabberbot'))
 import blabberbot
 
-INTERVAL = 5
+INTERVAL = 60*60*6
 
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
@@ -14,7 +14,9 @@ ACCESS_SECRET = os.environ['ACCESS_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY,ACCESS_SECRET) 
 api = tweepy.API(auth)
-bot = blabberbot.Blabberbot(blabberbot.corpus)
+
+bot = blabberbot.Blabberbot(blabberbot.sheldon)
+bot.add_corpus(blabberbot.chandler)
 
 while True:    
     tweet = bot.generate_tweet(140)
--- blabberbot/blabberbot.py
+++ blabberbot/blabberbot.py
@@ -1,12 +1,20 @@
 import markovify
+import posified
 
 class Blabberbot():
     def __init__(self,filepath):
         with open(filepath) as file:
             text = [line.strip() for line in file.readlines()]
-        self.text_model = markovify.Text(text)
+        self.text_model = posified.POSifiedText(text)
+    
+    def add_corpus(self,filepath):
+        with open(filepath) as file:
+            text = [line.strip() for line in file.readlines()]
+        new_model = posified.POSifiedText(text)
+        self.text_model = markovify.combine([self.text_model,new_model])
 
     def generate_tweet(self,n):
         return self.text_model.make_short_sentence(n)
 
-corpus = "/app/Tweets/final.txt"
+sheldon = "/app/Tweets/sheldonfinal.txt"
+chandler = "/app/Tweets/chandlerfinal.txt"
