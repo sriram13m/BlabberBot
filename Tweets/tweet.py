7b95957d1f2c73acbbd9995b167f5d04242912c5
Sriram <sriram13k@gmail.com>
2018-12-09 13:34:42 +0530

    Update tweet.py

--- Tweets/tweet.py
+++ Tweets/tweet.py
@@ -5,7 +5,7 @@ import sys,os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'blabberbot'))
 import blabberbot
 
-INTERVAL = 5
+INTERVAL = 60*60*6
 
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
