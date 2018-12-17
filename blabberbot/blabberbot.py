f136a2375dd3d1d4502f74fc2fd5cdad315367c1
Sriram <sriram13k@gmail.com>
2018-12-17 20:18:57 +0530

    Revert "Updated Corpus" Changed This reverts commit 4339eb0a598d1978a42157b9ded595fa47d663ac.

--- blabberbot/blabberbot.py
+++ blabberbot/blabberbot.py
@@ -7,23 +7,16 @@ class Blabberbot():
     def __init__(self,filepath):
         with open(filepath) as file:
             text = [line.strip() for line in file.readlines()]
-        self.text_model = markovify.NewlineText(text)
-        self.text_model_size = len(text)
+        self.text_model = posified.POSifiedText(text)
     
     def add_corpus(self,filepath):
         with open(filepath) as file:
             text = [line.strip() for line in file.readlines()]
-        new_model = markovify.NewlineText(text)
-        corpus_size = len(text)
-        if self.text_model_size > corpus_size:
-            ratio1 = 1.0
-            ratio2 = float(corpus_size)/float(self.text_model_size)
-        else :
-            ratio2 = 1.0
-            ratio1 = float(self.text_model_size)/float(corpus_size)
-        self.text_model = markovify.combine([self.text_model,new_model],[ratio1,ratio2])
+        new_model = posified.POSifiedText(text)
+        self.text_model = markovify.combine([self.text_model,new_model])
+
     def generate_tweet(self,n):
         return self.text_model.make_short_sentence(n)
 
-sheldon = "/home/fevenz/Sriram/Academic/Packages/Blabberbot/BlabberBot/Tweets/sheldonfinal.txt"
-chandler = "/home/fevenz/Sriram/Academic/Packages/Blabberbot/BlabberBot/Tweets/chandlerfinal.txt"
+sheldon = "/app/Tweets/sheldonfinal.txt"
+chandler = "/app/Tweets/chandlerfinal.txt"
